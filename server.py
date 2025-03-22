2f8eac1560ea0dfef3f61aa9807a237beddc28b3
Pranay-Gupta <pranaygupta2022@gmail.com>
2025-03-23 02:41:48 +0530

    Fourth Jenkins

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["Get"])
 def root():
-  return "Welcom to the server Pranay Gupta"
+  return "Welcome to the server Pranay G Gupta"
 
 @app.route("/version", methods=["Get"])
 def version():
